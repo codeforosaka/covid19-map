a8561b70053af9a5fdb6dbd3550de74b1d413c5f
teppei696 <teppei6969@gmail.com>
2022-04-16 15:21:51 +0900

    一旦コミット

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,70 @@
-require("dotenv").config();
-const { TOKEN } = process.env;
+require('dotenv').config()
+const { TOKEN } = process.env
 
-var env = process.env.NODE_ENV || "development";
-if (env === "development" || env === "test") {
-  var API_URL = "http://localhost:3000";
+var env = process.env.NODE_ENV || 'development'
+if (env === 'development' || env === 'test') {
+  var API_URL = 'http://localhost:3000'
 } else {
-  var API_URL = process.env.API_URL;
+  var API_URL = process.env.API_URL
 }
 
 export default {
-  ssr: "true", //or spa
+  ssr: 'true', //or spa
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "大阪府感染防止認証ゴールドステッカーマップ",
+    title: '大阪府感染防止認証ゴールドステッカーマップ',
     htmlAttrs: {
-      lang: "ja"
+      lang: 'ja'
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~assets/global.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '@/plugins/vuetify.ts',
+      ssr: true
+    }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ['@nuxtjs/vuetify', '@nuxt/typescript-build'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    "@nuxtjs/axios",
+    '@nuxtjs/axios',
     [
-      "@nuxtjs/google-gtag",
+      '@nuxtjs/google-gtag',
       {
-        id: "G-9GEZKPR44Q",
+        id: 'G-9GEZKPR44Q',
         debug: true
       }
     ]
   ],
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    defaultAssets: {
+      icons: false
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extend(config, ctx) {
       config.node = {
-        fs: "empty"
-      };
+        fs: 'empty'
+      }
     }
   },
 
@@ -61,8 +72,8 @@ export default {
     TOKEN
   },
 
-  serverMiddleware: ["~/api"],
+  serverMiddleware: ['~/api'],
   axios: {
     baseURL: API_URL
   }
-};
+}
